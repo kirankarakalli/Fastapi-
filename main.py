a79b3f576b8ade1ec8f8ef25d5fbe26ae587bb5a
kiran <kiran.karakalli92@gmail.com>
2026-01-31 12:29:11 +0530

    basic operations of fastapi

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,HTTPException,Query
 import json
 app=FastAPI()
 
@@ -21,3 +21,26 @@ def viewdata():
     data=read_json()
 
     return data
+
+@app.get('/patient/{id}')
+def patient(id:str=Path(...,description='ID of the patient in the DB',example='P001')):
+    data=read_json()
+
+    if id in data:
+        return data[id]
+    
+    raise HTTPException(status_code=404,detail='patient not found')
+
+
+@app.get('/sort')
+def sort_patients(sort_by:str=Query(...,description='sort on the basis of height,weight and bmi'),order:str=Query('asc',description='sort in asc or desc order')):
+    valid_fields=['heights','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail='Invalid field select from {valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail='Invalid order select bw asc and desc')
+    
+    data=read_json()
+    sorted_data=sorted(data.values(),key=lambda x:x.get('sort_by',0),reverse=False)
+    return sorted_data
